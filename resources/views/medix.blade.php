315d29eb58c58e18eee9a1dd710c32120d3eb272
Jose Emmanuel Manangan <noel1231@users.noreply.github.com>
2016-11-22 10:57:39 +0800

    Homepage created using Materialize CSS

---
+++ resources/views/medix.blade.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Medix Webinar Website</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="{{ URL::to('css/materialize.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="{{ URL::to('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <div class="navbar-fixed">
+    <nav class="white" role="navigation">
+      <div class="container">
+        <div class="nav-wrapper" style="font-family: Oxygen; font-weight: bold;">
+          <a id="logo-container" href="#" class="brand-logo">
+            <img src="{{ URL::to('img/logo.png') }}">
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#">TOP COURSES</a></li>
+            <li><a href="#">NEW COURSES</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">LOG IN</a></li>
+            <li><a href="#" class="btn">SIGN UP</a></li>
+            
+          </ul>
+
+          <ul id="nav-mobile" class="side-nav">
+            <li><a href="#">TOP COURSES</a></li>
+            <li><a href="#">NEW COURSES</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">LOG IN</a></li>
+            <li><a href="#" class="btn waves-effect webinar-color">SIGN UP</a></li>
+          </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+  <div class="section">
+    <div class="container">
+      <br>
+      <br>
+      <h4 class="header center">ONLINE COURSES FOR <span style="font-family: Oxygen; font-weight: bold; color: #1b584c;">CONTINUING EDUCATION</span></h4>
+
+      <div class="row center">
+        <div class="col s8 offset-s2">
+          <h6 style="font-family: Source Sans Pro;">Blessed is the man who doesn't walk in the counsel of the wicked, nor stand in the way of sinners, nor sit in the seat of scoffers; but his delight is in Yahweh's law.</h6>
+        </div>
+      </div>
+      
+      <div class="row center">
+        <a href="#" id="download-button" class="btn-large waves-effect waves-light">Get Started</a>
+      </div>
+    </div>
+  </div>
+
+    <div class="divider"></div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col s5">
+            <h4>LATEST UPLOADS</h4>
+            <div style="font-family: Source Sans Pro;">Save me, God by your name. Vindicate me in your might. Hear my prayer, God. Listen to the words of my mouth. For strangers have risen up against me. Violent men have sought after my soul. They haven't set God before them.</div>
+            <div class="divider"></div>
+
+            <h5>NANO-TECHNOLOGY IN DENTAL INDUSTRY</h5>
+            <p>By Chapin Aaron Harris</p>
+            <p>I will lift up my eyes to the hills. Where does my help come from? My help comes from Yahweh, who made heaven and earth.</p>
+          
+            <div class="row center">
+              <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">VIEW COURSE</a>
+            </div>
+
+            <ul class="pagination">
+              <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+              <li class="waves-effect"><a href="#!">1</a></li>
+              <li class="waves-effect"><a href="#!">2</a></li>
+              <li class="waves-effect"><a href="#!">3</a></li>
+              <li class="waves-effect active"><a href="#!">4</a></li>
+              <li class="waves-effect"><a href="#!">5</a></li>
+              <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+            </ul>
+          </div>
+
+          <div class="col s7">
+            <img src="{{ URL::to('img/medic-hospital-laboratory-medical-40559.gif') }}" width="100%">
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="section">
+          <h4>DEAN'S WEEKLY PICS</h4>
+          <p>Many times they have afflicted me from my youth up. Let Israel now say, many times they have afflicted my from my youth up, yet they have not prevailed against me. The plowers plowed on my back. They made their furrows long. Yahweh is righteous.</p>
+        </div>
+
+        <div class="section">
+          <div class="row">
+
+            <div class="col s12 m7">
+              <div class="card">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/pexels-photo-87346.jpg') }}">
+                  <span class="card-title">Accelerated Aligner Therapy</span>
+                </div>
+                <div class="card-action">
+                  <a href="#">This is a link</a>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m5">
+              <div class="row">
+                <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="{{ URL::to('img/dentist-pain-borowac-cure-52527.jpg') }}">
+                      <span class="card-title">Tooth Structure: Nature&rsquo;s Best Restorative Material</span>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="{{ URL::to('img/woman-child-doctor-hospital.jpg') }}">
+                      <span class="card-title">Oral Health and Dental Management for the Pregnant Patient</span>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              <div class="row">
+                <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="{{ URL::to('img/pexels-photo.jpg') }}">
+                      <span class="card-title">Dry Mouth Discomfort Treatment</span>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col s12 m6">
+                  <div class="card">
+                    <div class="card-image">
+                      <img src="{{ URL::to('img/pexels-photo-127873.jpg') }}">
+                      <span class="card-title">Anterior Provisionalization Made Easy</span>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">This is a link</a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div
+              
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+  <div class="section">
+    <h4 class="center">TOP SPECIALIZATIONS</h4>
+
+      <div class="row">
+        <div class="col s12">
+          <ul class="tabs tabs-fixed-width">
+            <li class="tab"><a class="active" href="#test1">All</a></li>
+            <li class="tab"><a href="#test2">Endodontics</a></li>
+            <li class="tab"><a href="#test3">Orthodontics</a></li>
+            <li class="tab"><a href="#test4">Pediatrics</a></li>
+            <li class="tab"><a href="#test5">Prothodontics</a></li>
+            
+          </ul>
+        </div>
+
+        <div id="test1" class="col s12">
+          <div class="row">
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/img-root-canal.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/03.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/dentalimplants.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/Pediatric-Dentistry-1.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="row">
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/IMG_1806.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/bigstock-x-ray-photo-of-teeth-Dental-e-64462231.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/endodontics.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+            <div class="col s12 m3">
+              <div class="card small">
+                <div class="card-image">
+                  <img src="{{ URL::to('img/maxresdefault.jpg') }}">
+                  <span class="card-title">Card Title</span>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+        <div id="test2" class="col s12">Test 2</div>
+        <div id="test3" class="col s12">Test 3</div>
+        <div id="test4" class="col s12">Test 4</div>
+        <div id="test5" class="col s12">Test 4</div>
+        
+      </div>
+    </div>
+
+    <div class="">
+      <div class="row center">
+          <a href="#" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">VIEW ALL COURSES</a>
+        </div>
+    </div>
+  </div>
+
+  <div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m6">
+          <h2>ABOUT DENTAL ACCESS WEBINARS</h2>
+        </div>
+        <div class="col s12 m6">
+          Let God arise! Let his enemies be scattered! Let them who hate him also flee before him. As smoke is driven away, so drive them away. As was melts before the fire, so let the wicked perish at the presence of God. But let the righteous be glad. Let them rejoice before God. Yes, let them rejoice with gladness. Sing to God! Sing praises to his name! Extol him who rides on the clouds:to Yah, his name! Rejoice before him! A father of the fatherless, and a defender of the widows, is God in his holy habitation. God sets the lonely in families. He brings out the prisoners with singing, but the rebellious dwell in a sun-scorched land.
+
+          <div class="divider"></div>
+          <h4>EDUCATION IS KEY TO OUR DENTAL COMMUNITY'S DEVELOPMENT</h4>
+        </div>
+        
+      </div>
+    </div>
+
+  </div>
+
+  <footer class="page-footer teal">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Company Bio</h5>
+          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Settings</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Connect</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Link 1</a></li>
+            <li><a class="white-text" href="#!">Link 2</a></li>
+            <li><a class="white-text" href="#!">Link 3</a></li>
+            <li><a class="white-text" href="#!">Link 4</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      </div>
+    </div>
+  </footer>
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="{{ URL::to('js/materialize.js') }}"></script>
+  <script src="{{ URL::to('js/init.js') }}"></script>
+
+  </body>
+</html>
