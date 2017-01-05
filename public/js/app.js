b9732bbbc113f821b93c8fb2763ba5e7f4db605e
micinfo <michael.alumno@gmail.com>
2017-01-05 13:59:06 +0800

    about description changed

--- public/js/app.js
+++ public/js/app.js
@@ -65351,11 +65351,6 @@
 	                                                'h3',
 	                                                { style: { fontFamily: "Source Sans Pro" } },
 	                                                'LATEST UPLOADS'
-	                                            ),
-	                                            _react2.default.createElement(
-	                                                'strong',
-	                                                { style: { fontFamily: 'Oxygen' } },
-	                                                'NANO-TECHNOLOGY IN DENTAL INDUSTRY'
 	                                            )
 	                                        ),
 	                                        _react2.default.createElement('hr', null),
@@ -65380,7 +65375,7 @@
 	                                            _react2.default.createElement(
 	                                                'p',
 	                                                { className: 'webinarDescription' },
-	                                                'The current trent of the dental profession is geared towards esthetics and beauty.'
+	                                                'The current trend of the dental profession is geared towards esthetics and beauty.'
 	                                            ),
 	                                            _react2.default.createElement(
 	                                                _reactRouter.Link,
