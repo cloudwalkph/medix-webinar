2f67d2c0d3a0454d62093549bb145e7870e4daf6
Jose Emmanuel Manangan <noel1231@users.noreply.github.com>
2017-02-15 18:58:12 +0800

    Webinar - update reminder email

--- resources/views/emails/reminder.blade.php
+++ resources/views/emails/reminder.blade.php
@@ -1,14 +1,19 @@
 <p>As an attendee, you will need a computer with audio, a high-speed Internet connection and Adobe Flash player, which is already installed in 99% of computers.
 </p><p>You will receive log-in instructions within 24 hours of the commencement of the event.
-</p><br />
+</p>
+<br />
 <p>Please do not distribute your user-id and password to other users. You may not allow others to use your login information.  Accounts used by multiple users will be tracked and deleted. Refunds will not be issued for accounts, which have been terminated due to misuse.
-</p><br />
+</p>
+<br />
 <p>In the event that Dental Access feels it is necessary to cancel this event due to circumstances beyond our control, attempts to contact all registrants will be made at the time cancellation is determined. A rescheduled date will be confirmed as soon as possible.
 </p><p>If you have any problems, please contact Dr Arvin Laguna or Dr Armi Cabero, at: email: media@identalaccess.com or arvin.laguna@identalaccess.com
-</p><br />
-<p>Thank you for your participation,
-</p><br />
-<p>Dental Access Publications Inc
 </p>
+<br />
 <p>You may follow your webinar at this link</p>
 <a href="http://medix-webinar.dev/courses/{{$course_id}}?uid={{$user_id}}">Click here</a>
+<br />
+<p>Thank you for your participation,
+</p>
+<br />
+<p>Dental Access Publications Inc
+</p>
